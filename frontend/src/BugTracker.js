c9076ecb13103459018b82ff2ba5ec608478760f
Matthew Maloney <matttmaloney@gmail.com>
2019-12-10 17:50:36 -0500

    corrected initial state

--- frontend/src/BugTracker.js
+++ frontend/src/BugTracker.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import BugList from './BugList';
-import bugs from './TestData';
+import { getTestBugs } from './TestData';
 
 export default class BugTracker extends React.Component {
-  constructor() {
+  constructor(props) {
+    super(props);
     this.state = {
-      bugs
+      bugList: []
     }
+  }
+
+  componentDidMount() {
+    this.setState( { bugList: [...getTestBugs()] } );
   }
 
   render() {
     return (
         <div className="App">
           <h2>Bug Tracker</h2>
-          <BugList bugList={this.state} />
+          <BugList bugList={this.state.bugList} />
         </div>
       );
     }
