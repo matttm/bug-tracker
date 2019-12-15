967415f8de36430e98ad76e20372433145b4482d
Matthew Maloney <matttmaloney@gmail.com>
2019-12-15 15:29:11 -0500

    Added non-functional bootstrap button

--- frontend/src/BugList.js
+++ frontend/src/BugList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Bug from './Bug';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 export default class BugList extends React.Component {
     constructor(props) {
@@ -25,9 +27,16 @@ export default class BugList extends React.Component {
         });
 
         return (
+            <>
+            <DropdownButton id="dropdown-item-button" title="Filter">
+                <Dropdown.Item as="button" >Name</Dropdown.Item>
+                <Dropdown.Item as="button" >Date</Dropdown.Item>
+                <Dropdown.Item as="button" >Priority</Dropdown.Item>
+            </DropdownButton>
             <ul>
                 {formattedBugList}
             </ul>
+            </>
         );
     }
 }
