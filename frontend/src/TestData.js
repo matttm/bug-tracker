2d477571731d0ee39bfa6b88e5c5936a38062612
Matthew Maloney <matttmaloney@gmail.com>
2019-12-29 13:34:53 -0500

    added test data entry

--- frontend/src/TestData.js
+++ frontend/src/TestData.js
@@ -4,7 +4,7 @@ export function getTestBugs() {
             id: 1,
             name: "Context Menu is broken",
             desc: "Right-click generates an event, but the context menu is not shown",
-            priority: 4,
+            priority: 5,
             date: "10-10-19",
             reporter: "Matt Maloney"
         },
@@ -12,8 +12,16 @@ export function getTestBugs() {
             id: 2,
             name: "Tracks are not being registered",
             desc: "Track entities are being displayed but are not being registered in the event registration systenm",
-            priority: 5,
+            priority: 3,
             date: "10-10-19",
+            reporter: "Matt Maloney"
+        },
+        {
+            id: 3,
+            name: "Consolidate CSS naming scheme",
+            desc: "The CSS file being used contains classes of varying prefixes. Refactor these classes to conform to one prefix",
+            priority: 1,
+            date: "11-11-19",
             reporter: "Matt Maloney"
         },
     ];
