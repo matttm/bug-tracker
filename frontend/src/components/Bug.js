cfa5d4ab4736741d3fb6417dab25294a129f2032
Matthew Maloney <matttmaloney@gmail.com>
2020-01-12 11:47:57 -0500

    Wrote BugDesc to use bootstrap modal

--- frontend/src/components/Bug.js
+++ frontend/src/components/Bug.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import BugDesc from './BugDesc';
 
 export default class Bug extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        };
+    }
+
+    handleOpen = () => this.setState({show: true });
+    handleClose = () => this.setState({show: false });
+
     render() {
         return (
-            <div className="bug">
-                <div className='bugInfo'>
-                    <div className='bugName'>
-                        {this.props.name}
-                    </div>
-                    <div className="bugDate">
-                        {this.props.date}
+            <>
+                <div className="bug" onClick={this.handleOpen}>
+                    <div className='bugInfo'>
+                        <div className='bugName'>
+                            {this.props.name}
+                        </div>
+                        <div className="bugDate">
+                            {this.props.date}
+                        </div>
                     </div>
                 </div>
-            </div>
+                <Modal show={this.state.show} onHide={this.handleClose} animation={false}>
+                    <BugDesc />
+                </Modal>
+            </>
         );
     }
 }
--- frontend/src/components/BugDesc.js
+++ frontend/src/components/BugDesc.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
 
 export default class BugDesc extends React.Component {
     constructor(props) {
         super(props);
-        this.portalRoot = document.getElementById("portal");
-        this.el = document.createElement("div");
-        this.el.className = "portal";
+        // this.portalRoot = document.getElementById("portal");
+        // this.el = document.createElement("div");
+        // this.el.className = "portal";
     }
 
-    componentDidMount() {
-        this.portalRoot.appendChild(this.el);
-    }
+    // componentDidMount() {
+    //     this.portalRoot.appendChild(this.el);
+    // }
 
-    componentWillUnmount() {
-        this.portalRoot.removeChild(this.el);
-    }
+    // componentWillUnmount() {
+    //     this.portalRoot.removeChild(this.el);
+    // }
     
-    _handleSubmit = () => this.props.handleSubmit({...this.state});
 
     render() {
-        let form = (
+      return (
             <React.Fragment>
+                 <div className="form-close">
+                    <FontAwesomeIcon
+                        icon={faTimesCircle}
+                        onClick={this.props.toggleForm}>
+                    </FontAwesomeIcon>
+                    Testing
+                </div>
             </React.Fragment>
         );
-        return ReactDOM.createPortal(form, this.el);
+        // return ReactDOM.createPortal(form, this.el);
     }
 }
