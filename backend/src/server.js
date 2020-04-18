d2cb8dfc08fddeedd53088d11f86055b73917920
Matthew Maloney <matttmaloney@gmail.com>
2020-04-18 14:00:16 -0400

    configuring server to use lowdb

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,11 @@ const PORT = 3000;
 const app = express();
 const router = express.Router();
 
+db.defaults({ bugs: [], count: 0 })
+    .write();
+
+let updateCount = () => db.update('count', n => n + 1);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -18,18 +23,25 @@ router.get('/test', (req, res) => {
 });
 
 router.get('/bugs', (req, res) => {
-    res.json({
-        id: 6,
-        name: "Test path in backend doesn't work",
-        desc: "GET requests to '/test' is not being received. The backend's middleware may be the problem",
-        priority: 3,
-        date: "2020-01-22",
-        reporter: "Joey Bagodonuts"
-    });
+    res.json(
+        db.get('bugs')
+            .value()
+    );
 })
 
-router.post('/bug', (req, res) => {
+router.post('/bugs', (req, res) => {
     const bug = req.body;
+    db.get('bugs')
+        .push({
+            id: db.get('count').value(),
+            name: bug.name,
+            desc: bug.desc,
+            priority: bug.priority,
+            date: bug.date,
+            reporter: bug.reporter
+        })
+        .write();
+	updateCount();
 });
 
 app.use('/api', router);
