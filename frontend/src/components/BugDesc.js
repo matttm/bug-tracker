a39ed5562aa08292a088959cb5f7c33ea43c37f0
Matthew Maloney <matttmaloney@gmail.com>
2020-07-11 15:45:04 -0400

    fixed modal titling issue on edits/descs

--- frontend/src/components/BugDesc.js
+++ frontend/src/components/BugDesc.js
@@ -38,9 +38,6 @@ export default class BugDesc extends React.Component {
             <>
             <div className="flex-grid">
                 <div className="col">
-                    Priority: {this.props.name}
-                </div>
-                <div className="col">
                     Priority: {this.props.priority}
                 </div>
                 <div className="col">
@@ -69,12 +66,12 @@ export default class BugDesc extends React.Component {
 
     render() {
         let content = this.getContent();
-
+        let title = this.state.isEdit ? "Edit Bug" : this.props.name;
       return (
         <GenericModal
           show={this.props.show}
           onHide={this.props.onHide}
-          title="Edit Bug"
+          title={title}
         >
             <Container fluid>
                 {content}
