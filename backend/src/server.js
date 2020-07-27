e23ac073b78828fe44c7d36f05e061789690fec7
Matthew Maloney <matttmaloney@gmail.com>
2020-07-27 15:46:52 -0400

    trying to configure env

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var low        = require( "lowdb");
 var FileSync   = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("resources/db.json");
+const adapter = new FileSync(`${process.env.DB_FILE}`);
 const db      = low(adapter);
 
 const app    = express();
